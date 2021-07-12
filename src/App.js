8c9fb576437815af4a3313181d0d8c01d0532b11
Dedi-edot <id.dedi81@gmail.com>
2021-07-12 10:58:51 +0700

    fitur Edit Data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import Axios from "axios";
 
 class App extends React.Component {
   state = {
+    edit: false,
     dataApi: [],
+    newDataApi: {
+      id: 0,
+      title: "",
+      author: "",
+    },
   };
 
   renderDataApi = () => {
@@ -20,6 +26,58 @@ class App extends React.Component {
     });
   };
 
+  handleChange = (e) => {
+    let newData = { ...this.state.newDataApi };
+    if (this.state.newDataApi.edit === false) {
+      newData["id"] = new Date().getTime();
+    }
+    newData[e.target.name] = e.target.value;
+    this.setState({ newDataApi: newData });
+  };
+
+  clearData = () => {
+    let newData = { ...this.state.newDataApi };
+    newData["id"] = 0;
+    newData["title"] = "";
+    newData["author"] = "";
+    this.setState({ newDataApi: newData });
+    this.setState({ edit: false });
+  };
+
+  handleAddData = () => {
+    Axios.post(`http://localhost:2000/posts/`, this.state.newDataApi)
+      .then((res) => {
+        alert("Add Data Berhasil");
+        this.clearData();
+        this.renderDataApi();
+      })
+      .catch((err) => {
+        alert("Terjadi masalah di server");
+      });
+  };
+
+  handleMenuEdit = (e) => {
+    this.setState({ edit: true });
+    Axios.get(`http://localhost:2000/posts/${e.target.value}`).then((res) => {
+      this.setState({ newDataApi: res.data });
+    });
+  };
+
+  handleEditData = () => {
+    Axios.put(
+      `http://localhost:2000/posts/${this.state.newDataApi.id}`,
+      this.state.newDataApi
+    )
+      .then((res) => {
+        alert("Edit Data Berhasil");
+        this.renderDataApi();
+        this.clearData();
+      })
+      .catch((err) => {
+        alert("Terjadi masalah di server");
+      });
+  };
+
   componentDidMount() {
     this.renderDataApi();
   }
@@ -28,6 +86,49 @@ class App extends React.Component {
     return (
       <div>
         <h1>Hello API</h1>
+        {!this.state.edit ? (
+          <>
+            <strong>Tambah Data</strong> <br />
+            <input
+              type="text"
+              name="title"
+              value={this.state.newDataApi.title}
+              placeholder="Input Title"
+              onChange={this.handleChange}
+            />
+            <br />
+            <input
+              type="text"
+              name="author"
+              value={this.state.newDataApi.author}
+              placeholder="Input Author"
+              onChange={this.handleChange}
+            />
+            <br />
+            <button onClick={this.handleAddData}>Add Data</button>
+          </>
+        ) : (
+          <>
+            <strong>Edit Data</strong> <br />
+            <input
+              type="text"
+              name="title"
+              value={this.state.newDataApi.title}
+              placeholder="Input Title"
+              onChange={this.handleChange}
+            />
+            <br />
+            <input
+              type="text"
+              name="author"
+              value={this.state.newDataApi.author}
+              placeholder="Input Author"
+              onChange={this.handleChange}
+            />
+            <br />
+            <button onClick={this.handleEditData}>Save Data</button>
+          </>
+        )}
         {this.state.dataApi.map((data, index) => {
           return (
             <div key={index}>
@@ -36,6 +137,9 @@ class App extends React.Component {
               <p>{data.author}</p>
               <button value={data.id} onClick={this.handleDelete}>
                 Delete
+              </button>
+              <button value={data.id} onClick={this.handleMenuEdit}>
+                Edit
               </button>
             </div>
           );
